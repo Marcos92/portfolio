4d4aa63e4b11e52f45dacb63c307f8bf998ac73c
Marcos Rodrigues <marcos.mgbr@gmail.com>
2025-06-09 18:10:06 +0100

    Update with Boltspitter

--- src/data.ts
+++ src/data.ts
@@ -4,8 +4,8 @@ import { Project } from "./Project.ts";
 
 export const projects = [
     new Project(
-      "Unannounced roguelike FPS",
-      "I'm currently developing a roguelike first-person shooter inspired by Vampire Survivors, classic boomer shooters, and fast-paced combo systems seen in games like Tony Hawk's Pro Skater and Devil May Cry. Made with Unity, with a release planned in 2026.",
+      "Boltspitter",
+      "I'm currently developing a roguelike first-person shooter inspired by Vampire Survivors, classic boomer shooters, and fast-paced combo systems seen in games like Tony Hawk's Pro Skater and Devil May Cry. Made with Unity, with a release planned in 2026.<br><br>Follow the development <a href='https://bsky.app/profile/boltspitter.bsky.social' target='_blank'><b>here</b></a>!",
       "assets/fps logo.png",
       "assets/fps.gif",
       "#AA0000",
@@ -23,7 +23,7 @@ export const projects = [
     ),
     new Project(
       "Project Ghosts<br>(working title)",
-      "A co-op party game for up to four players. Inspired by games such as Overcooked or Moving Out, but with ghost hunting. Developed all by myself using Unity.<br><br>While the project is currently on hold, you can follow the development <a href='https://twitter.com/ProjectGhosts/' target='_blank'><b>here</b></a>.",
+      "A co-op party game for up to four players. Inspired by games such as Overcooked or Moving Out, but with ghost hunting. Developed all by myself using Unity.<br><br>While the project is currently on hold, you can follow the development <a href='https://twitter.com/ProjectGhosts/' target='_blank'><b>here</b></a>!",
       "assets/pg logo.webp",
       "assets/ghosts.gif",
       "#6600FF",
@@ -32,7 +32,7 @@ export const projects = [
     ),
     new Project(
       "Starkiller",
-      "Developed for a college class, this is a retro-inspired 2D roguelike shooter. Made with MonoGame, I did all the game design, programming, art, and music.<br><br>It was released on itch.io and you can download it <a href='https://marcos92.itch.io/starkiller/' target='_blank'><b>here</b></a> for free.",
+      "Developed for a college class, this is a retro-inspired 2D roguelike shooter. Made with MonoGame, I did all the game design, programming, art, and music.<br><br>It was released on itch.io and you can download it <a href='https://marcos92.itch.io/starkiller/' target='_blank'><b>here</b></a> for free!",
       "assets/sk logo.png",
       "assets/starkiller.gif",
       "#110033",
@@ -41,7 +41,7 @@ export const projects = [
     ),
     new Project(
       "Agar.io",
-      "During my time at Miniclip, this was the project I worked on the longest. From 2016 to 2019, I developed countless content updates for the browser version, including major features like the Battle Royale mode and the Skin Creator. The game was made using standard web technologies (HTML, CSS, and JavaScript) along with Vue.js, and also included components written in C++ and Haxe.<br><br>Play it <a href='https://agar.io/' target='_blank'><b>here</b></a>.",
+      "During my time at Miniclip, this was the project I worked on the longest. From 2016 to 2019, I developed countless content updates for the browser version, including major features like the Battle Royale mode and the Skin Creator. The game was made using standard web technologies (HTML, CSS, and JavaScript) along with Vue.js, and also included components written in C++ and Haxe.<br><br>Play it <a href='https://agar.io/' target='_blank'><b>here</b></a>!",
       "assets/agario logo.png",
       "assets/agario.gif",
       "#FFFFFF",
@@ -50,7 +50,7 @@ export const projects = [
     ),
     new Project(
       "Soccer Stars",
-      "Responsible for the development of new features and maintenance on the mobile version of the game. Developed using Objective-C.<br><br>Available on <a href='https://play.google.com/store/apps/details?id=com.miniclip.soccerstars' target='_blank'><b>Android</b></a> and <a href='https://apps.apple.com/pt/app/soccer-stars/id826523703' target='_blank'><b>iOS</b></a>.",
+      "Responsible for the development of new features and maintenance on the mobile version of the game. Developed using Objective-C.<br><br>Available on <a href='https://play.google.com/store/apps/details?id=com.miniclip.soccerstars' target='_blank'><b>Android</b></a> and <a href='https://apps.apple.com/pt/app/soccer-stars/id826523703' target='_blank'><b>iOS</b></a>!",
       "assets/ss logo.png",
       "assets/ss.gif",
       "#339900",
@@ -59,7 +59,7 @@ export const projects = [
     ),
     new Project(
       "8 Ball Pool",
-      "Occasional support development on the Facebook Messenger version of the game. Developed using Typescript.<br><br>Play it <a href='https://www.facebook.com/gaming/play/1829935823906294' target='_blank'><b>here</b></a>.",
+      "Occasional support development on the Facebook Messenger version of the game. Developed using Typescript.<br><br>Play it <a href='https://www.facebook.com/gaming/play/1829935823906294' target='_blank'><b>here</b></a>!",
       "assets/8bp logo.png",
       "assets/8bp.gif",
       "#121264",
@@ -68,7 +68,7 @@ export const projects = [
     ),
     new Project(
       "Bowmasters",
-      "Responsible for porting new features to the Android version of the game, using Unity.<br><br>Play it <a href='https://play.google.com/store/apps/details?id=com.playgendary.bowmasters' target='_blank'><b>here</b></a>.",
+      "Responsible for porting new features to the Android version of the game, using Unity.<br><br>Play it <a href='https://play.google.com/store/apps/details?id=com.playgendary.bowmasters' target='_blank'><b>here</b></a>!",
       "assets/bm logo.webp",
       "assets/bowmasters.gif",
       "#CCFF00",
@@ -77,7 +77,7 @@ export const projects = [
     ),
     new Project(
       "Badminton Clash 3D",
-      "Helped refining the core gameplay prototype, working closely with the game designer. Made using Unity.<br><br>Available on <a href='https://play.google.com/store/apps/details?id=games.onebutton.badminton' target='_blank'><b>Android</b></a> and <a href='https://apps.apple.com/vn/app/badminton-clash-3d/id6444045683' target='_blank'><b>iOS</b></a>.",
+      "I worked on the refinement of the the core gameplay prototype, adjusting the mechanics closely with the game designer. Made using Unity.<br><br>Available on <a href='https://play.google.com/store/apps/details?id=games.onebutton.badminton' target='_blank'><b>Android</b></a> and <a href='https://apps.apple.com/vn/app/badminton-clash-3d/id6444045683' target='_blank'><b>iOS</b></a>!",
       "assets/bc logo.png",
       "assets/badminton.gif",
       "#0099FF",
